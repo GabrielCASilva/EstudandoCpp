0706d29fca7c89a07cbdde227c84f8b6ddd67652
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2025-04-02 20:07:21 -0300

    estrutura

--- _0_estrutura/main.cpp
+++ _0_estrutura/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello\n";
+    std::cout << "Hello World\n";
     return 0;
 }
+
+/*
+ * Função main();
+ * - Função que é chamada pelo sistema operacional
+ *   - argc -> quantidades de argumentos que vão ser passados
+ *   - *argv[] -> os argumentos em si
+ * - Retorno é sempre um inteiro
+ */
+
+/*
+ * Namespaces:
+ * - Nomes que são dados a partes do código para ajudar a reduzir conflitos
+ *   Ex:
+ *    - std::cin
+ *    - std::cout
+ */
+
+/*
+ * Básico de I/O
+ * cout - saida para o console
+ * cin  - fluxo entrada do teclado
+ * <<   - operador de fluxo inserção
+ * >>   - operador de fluxo estração
+ */
